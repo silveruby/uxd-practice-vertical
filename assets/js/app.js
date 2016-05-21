8ceb48eb79ee28ef4f762f327fbdcd27ab807289
Ruby Zheng <zhengsan@gmail.com>
2016-05-21 23:32:13 +0200

    finished main functionalities

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,8 +33,8 @@
 				var myFirebaseRef = new Firebase(fireBaseProject);
 				return $firebaseAuth(myFirebaseRef);
 			},
-			obj  : function(userID){
-				var myFirebaseRef = new Firebase(fireBaseProject + userID);				
+			obj  : function(userNormalizedEmail){
+				var myFirebaseRef = new Firebase(fireBaseProject + userNormalizedEmail);				
 				return $firebaseObject(myFirebaseRef);
 			}
 		};
@@ -49,10 +49,17 @@
 
 		var vm = $scope;
 		vm.current_state = '';
+		vm.current_status = '';
+		vm.current_link = 'http://www.uxpvd.io/?email='
 		vm.selected_count = 0;
 		vm.pv1 = [];
 		vm.pv2 = [];
 		vm.select = [];	
+		vm.msgBox = { user: '', 
+					email: '', 
+					link: '',
+					password: '' };
+
 
 		/** Init
 		*/
@@ -61,59 +68,61 @@
 			// Detect URL parameter
 			vm.loc = $location.path('/').search();
 
-			console.log(vm.loc.email);
+			// Determine data is in FireBase
+			if(vm.loc.email != undefined && vm.loc.email != ''){	  	
+				  	
+				// Normalize Email
+			  	vm.msgBox.email = vm.loc.email;
+				//console.log(email);
+
+				// Try to get results based on email
+				myFireBase.obj(vm.normalizeEmail(vm.msgBox.email)).$loaded()
+					.then(function(results){
+
+					if(results.$value === null){
+
+						vm.msgBox.link = vm.current_link + vm.loc.email;
+
+						// Load default selection JSON object 
+						vm.loadDefaultSelection('is_error');
+
+						// Go to state 1
+						//vm.goToState('s1', 'is_error');
+					}
+					else{				
+						console.log(results);
+					    vm.select = results.select;
+					    vm.msgBox.user = results.name;
+					    vm.msgBox.link = vm.msgBox.link + vm.loc.email;
+
+					    for (pv in vm.select){
+							for (a in vm.select[pv]){
+								//console.log('a: ' + a);
+								//console.log('vm.select[pv[a]: ' + vm.select[pv][a]);
+								if(vm.select[pv][a]) vm.selected_count ++;
+							}
+						}
+
+						// Go to state 4
+						vm.goToState('s4', 'is_default');
+					}
+
+				}).catch(function(err) {
+					// Display error
+					vm.msgBox.link = vm.msgBox.link + vm.loc.email;
+
+					// Load default selection JSON object 
+					console.log("No record for this userID");
+					vm.loadDefaultSelection('is_error');
+				});				  	
+			} 
+			else{	
+				// Load default selection JSON object 
+				vm.loadDefaultSelection('is_default');	
+			} 
 
 			// Populate JSON chart
-			vm.populatePV();	
-
-			// Determine data is in FireBase
-			if(vm.loc.email != undefined){
-
-				// check if Email is valid
-				myFireBase.auth().$authWithPassword({
-					email: vm.loc.email,
-				  	password: 'nopassword'
-				}).then(function(authData) {
-				  	console.log("Logged in as:", authData.uid);
-				  	
-					// Try to get results based on user id
-					myFireBase.obj(authData.uid).$loaded()
-						.then(function(results){
-
-						if(results.$value === null){
-							// Load default selection JSON object 
-							vm.loadDefaultSelection();
-						}
-						else{				
-						    vm.select = results;
-
-						    for (pv in vm.select){
-								//console.log('pv: ' + pv);
-								for (a in vm.select[pv]){
-									//console.log('a: ' + a);
-									//console.log('vm.select[pv[a]: ' + vm.select[pv][a]);
-									if(vm.select[pv][a]) vm.selected_count ++;
-								}
-							}
-							vm.goToState('s4');
-						}
-
-					}).catch(function(err) {
-						// Load default selection JSON object 
-						console.log("User does not exist");
-						vm.loadDefaultSelection();
-					});				  	
-
-				}).catch(function(error) {				
-				  	// Load default selection JSON object 
-					console.log("UserID Select Object does not exist");
-					vm.loadDefaultSelection();	
-				});
-			} // end if
-			else{
-				// Load default selection JSON object 
-				vm.loadDefaultSelection();
-			} 
+			vm.populatePV();				
 		}; 	
 
 		/** State 1
@@ -122,7 +131,9 @@
 		*/
 		vm.toggleSelected = function(event, practiceVertical, areaOfInterest){
 
-			if(vm.current_state == 's1' || vm.current_state == 's2' || vm.current_state == 's6'){
+			if(vm.current_state == 's1' || 
+				vm.current_state == 's2' || 
+				vm.current_state == 's7'){
 				// Generate ripple effect
 				var areaBox, x, y, w, h, rect;
 
@@ -155,9 +166,9 @@
 				vm.select[practiceVertical.ClassName][areaOfInterest.ClassName] ? 
 					vm.selected_count++ : vm.selected_count--;
 
-				if (vm.current_state = 's1' && vm.selected_count > 0){
+				if (vm.current_state == 's1' && vm.selected_count > 0){
 					// switch state
-					vm.goToState('s2');		
+					vm.goToState('s2', 'is_default');		
 				}
 			}
 
@@ -171,49 +182,50 @@
 			// Sanitize name and email
 			// This is done via AngularJS Form... TBD
 			
-			// Generate random password
-			// var password = generatePassword();
+			// Update message box variables
+			vm.msgBox.password = 'password'
+			vm.msgBox.email = vm.newEmail;
+			vm.msgBox.user = vm.newName;
+			vm.msgBox.link = vm.current_link + vm.msgBox.email;
 
 			// Step 1: Create user
 			auth = myFireBase.auth();
 			auth.$createUser({
-				email: vm.newEmail,
-				password: "nopassword"
+				email: vm.msgBox.email,
+				password: vm.msgBox.password
 			}).then(function(userData) { 
 
 				// Step 2: Use userID to save selection
 			  	console.log("User " + userData.uid + " created successfully!");
-			  	
+
+			  	// Normalize email
+			  	normalizeEmail = vm.normalizeEmail(vm.msgBox.email);
+
 			  	// Save selection for new user
-			  	obj = myFireBase.obj(userData.uid);
+			  	obj = myFireBase.obj(normalizeEmail);
 				userSelectionJSON = vm.select;
 				angular.fromJson(angular.toJson(userSelectionJSON));
 
-				console.log(obj);
+				// Set value to firebase object
+				obj.select = userSelectionJSON;
+			  	obj.name = vm.msgBox.user;
 
-				obj.$ref().set(userSelectionJSON, function(error) {
-					if (error) {
-						// Presentation
-					  	$('.card-confirmation .error .msg').html("<strong>" + error + "</strong>");
-					  	$('.card-confirmation .success').hide();
-					  	$('.card-confirmation .error').show();
-					} else {
-						// Presentation
-					  	$('.card-confirmation .success').show();
-					  	$('.card-confirmation .error').hide();
-					}
+				obj.$save().then(function() {
+					// switch state
+					vm.goToState('s3', 'is_default');	
+				}, function(error) {
+					// If error saving JSON selection
+				  	$('.card-confirmation .error .msg').html("<strong>" + error + "</strong>");
+				  	// switch state
+					vm.goToState('s3', 'is_error');	
 				});
-
-			}).catch(function(error) { // oh no!
+			}).catch(function(error) {
 			  	console.log("Error: ", error);
-			  	// Presentation
+			  	// If error creating user account
 			  	$('.card-confirmation .error .msg').html("<strong>" + error + "</strong>");
-			  	$('.card-confirmation .success').hide();
-			  	$('.card-confirmation .error').show();			  	
-			});
-
-			// switch state
-			vm.goToState('s3');				
+			  	// switch state
+				vm.goToState('s3', 'is_error');			  	
+			});			
 		};
 
 		/** State 3
@@ -223,67 +235,73 @@
 		/** State 4
 		* Show user's link and share and edit options
 		*/		
-		vm.modifyChart = function(){
-
-		};		
 
 		/** State 5
 		* Authenticate email and password 
 		*/			
 		vm.authenticateUser = function(){
-			console.log("User email: " + vm.userEmail);	
+			console.log("User email: " + vm.msgBox.email);	
 
-			auth = myFireBase.auth();
-
-			auth.$authWithPassword({
-				email: vm.userEmail,
-			  	password: 'nopassword'
+			myFireBase.auth().$authWithPassword({
+				email: vm.msgBox.email,
+			  	password: vm.userPassword
 			}).then(function(authData) {
-			  	console.log("Logged in as:", authData.uid);
 
 			  	// Go to state modify
-			  	vm.goToState('s6');
+			  	vm.goToState('s7', 'is_default');
 
 			}).catch(function(error) {				
 			  	console.error("Authentication failed:", error);
 
 			  	// PRESENTATION
 			  	$('.card-authenticate .error .msg').html("<strong>" + error + "</strong>"); 	
-			  	$('.card-authenticate .form').hide();			  	
-			  	$('.card-authenticate .error').show();
+			  	// switch state
+				vm.goToState('s5', 'is_error');	
 			});
 		};
-		/** State 5 
+		/** State 6 
 		* Reset password
 		*/			
 		vm.resetUserPassword = function(){
-			console.log("User email: " + vm.userEmail);
-			console.log("User password: " + vm.userPassword);	
+			console.log("User password: " + vm.msgBox.email);	
 
-			auth = myFireBase.auth();
-
-			auth.$authWithPassword({
-				email: vm.userEmail,
-			  	password: vm.userPassword
+			myFireBase.auth().$resetPassword({
+				email: vm.msgBox.email
 			}).then(function(authData) {
-			  	console.log("Logged in as:", authData.uid);
-
-			  	// Go to state modify
-			  	vm.goToState('s6');
+			  	// Update with confirmation  message
+			  	$('.card-reset .success').show();
+			  	$('.card-reset .default').hide();
 
 			}).catch(function(error) {
-				
-			  	// Go to state modify
-			  	vm.goToState('s6');				
-			  	console.error("Authentication failed:", error);
+			  	// Update with confirmation  message
+			  	$('.card-reset .error').show();
+			  	$('.card-reset .default').hide();			
 			});
 		};		
 
-		/** State 6
-		* Save or cancel changes 
+		/** State 7
+		* Update or cancel changes 
 		*/	
 		vm.saveChanges = function(){
+		  	// Normalize email
+		  	normalizeEmail = vm.normalizeEmail(vm.msgBox.email);
 
+		  	// Update selection for new user
+		  	obj = myFireBase.obj(normalizeEmail);
+			userSelectionJSON = vm.select;
+			angular.fromJson(angular.toJson(userSelectionJSON));
+			obj.select = userSelectionJSON;
+			obj.name = vm.msgBox.user;
+
+			obj.$save().then(function() {
+				console.log("updated successfully");
+			 	// If error saving JSON selection
+				vm.goToState('s8', 'is_default');					
+			}, function(error) {
+			 	// If error saving JSON selection
+				vm.goToState('s8', 'is_error');	
+			});
+			
 		};
 
 		/** HELPER function
@@ -293,7 +311,6 @@
 			// Populate JSON chart
 			$http.get('/./assets/js/uxd-practice-verticals.json')
 			.success(function(results){
-				console.log('success');
 				vm.pv1 = results.slice(0,4);
 				vm.pv2 = results.slice(4,8);
 			})
@@ -305,9 +322,7 @@
 		/** HELPER function
 		* Load default selections
 		*/
-		vm.loadDefaultSelection = function(){
-
-			vm.goToState('s1');	
+		vm.loadDefaultSelection = function(status){	
 
 			$http.get('/./assets/js/default-selection.json')
 			.success(function(results){
@@ -318,26 +333,33 @@
 						if(vm.select[pv][a]) vm.selected_count++;
 					}
 				}
-				console.log('using default selection');
+				// Display default, no email detected
+				vm.goToState('s1', status);
 			})	
 			.error(function(){
 				console.log('error with default selection');
+
+				// Display default, no email detected
+				vm.goToState('s1', 'is_error');				
 			});			
 		};
 
 		/** HELPER function
 		* Switch between states
 		*/
-		vm.goToState = function(state){
+		vm.goToState = function(state, status){
 			
 			vm.current_state = state;
+			vm.current_status = status;
+
 			console.log("current state: " + vm.current_state);
+			console.log("current status: " + vm.current_status);
 
 			$('.fade').removeClass('fadein');
 
 			switch (state){
         		case 's1':
-        			$('.card-default').addClass('fadein');	
+        			$('.card-default').addClass('fadein');
         			break;
         		case 's2':
         			$('.card-save').addClass('fadein');	
@@ -346,23 +368,35 @@
         			$('.card-confirmation').addClass('fadein');	
         			break;	 
         		case 's4':
-        			console.log("state 4");
         			$('.messageBox .card-user').addClass('fadein');	
         			break;	
         		case 's5':
         			$('.card-authenticate').addClass('fadein');	
         			break;	
         		case 's6':
+        			$('.card-reset').addClass('fadein');	
+        			break;	        			
+        		case 's7':
         			$('.card-modify').addClass('fadein');	
-        			break;	        			        			           	
+        			break;	 
+        		case 's8':
+        			$('.card-updated').addClass('fadein');	
+        			break;	        			       			        			           	
         	} // end switch state
 		};
+
+		/** HELPER function
+		* Normalize email by removing special characters
+		*/
+		vm.normalizeEmail = function (email) {
+		    return email.replace(/[^a-zA-Z0-9]/g, '-');
+		} 	
 
 		/** HELPER function
 		* Generate random password
 		*/
 		vm.generatePassword = function () {
-		    var length = 8,
+		    var length = 5,
 		        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
 		        retVal = "";
 		    for (var i = 0, n = charset.length; i < length; ++i) {
@@ -371,7 +405,9 @@
 		    return retVal;
 		} // end helper function for generatePassword
 
-		vm.init();
+		angular.element(document).ready(function () {
+	        vm.init();
+	    });
 
 	}]); // end MainController
 
